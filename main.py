ab6d48c0b7c55b79648ecdecc99788053b41ad8d
Alisa Umyarova <alisa.umyarova@gmail.com>
2025-11-28 15:31:06 +0700

    manual input and test

---
+++ main.py
@@ -0,0 +1,10 @@
+from manual_input import input_array_manual
+
+def main():
+    print("-- Тест ручного ввода массива --")
+    arr = input_array_manual()
+    print("Вы ввели массив:", arr)
+
+if __name__ == "__main__":
+    main()
+
