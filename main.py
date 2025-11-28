248c01dcb26a817985603f2ed2b1361dfeb88dcb
Alisa Umyarova <alisa.umyarova@gmail.com>
2025-11-28 15:44:22 +0700

    sorting and test

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from random_array import generate_array
+from sorting import sort_array
 
 def main():
-    print("-- Тест генерации массива --")
-    result = generate_array(5, 1, 100)
-    print("Сгенерированный массив:", result)
+    print("-- Тест функции сортировки --")
+    arr = [5, 1, 3, 7, 2]
+    print("Исходный массив:", arr)
+    result = sort_array(arr)
+    print("После сортировки:", result)
 
 if __name__ == "__main__":
     main()
